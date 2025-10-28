09218f350dd339cbee2f70b0777989c8ebb976b9
Nazarii Tsyhaniuk <nazariyt@gmail.com>
2025-10-28 19:03:20 +0000

    feat(player): add PlayerEndpoints

---
+++ player-api-tests/src/main/java/com/ravejoy/player/players/PlayerEndpoints.java
@@ -0,0 +1,10 @@
+package com.ravejoy.player.players;
+
+public final class PlayerEndpoints {
+  private PlayerEndpoints() {}
+  public static final String CREATE = "/player/create/";
+  public static final String DELETE = "/player/delete/";
+  public static final String GET = "/player/get";
+  public static final String GET_ALL = "/player/get/all";
+  public static final String UPDATE = "/player/update/";
+}
