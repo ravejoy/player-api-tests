ca740b68bdde59815701e2b916334831b9477cf8
Nazarii Tsyhaniuk <nazariyt@gmail.com>
2025-10-29 01:10:49 +0000

    fix(http): remove custom Apache HttpClient factory to avoid RestAssured cast issue

--- src/main/java/com/ravejoy/player/http/RequestSpecFactory.java
+++ src/main/java/com/ravejoy/player/http/RequestSpecFactory.java
@@ -5,7 +5,6 @@ import static io.restassured.RestAssured.given;
 import com.ravejoy.player.config.AppConfig;
 import com.ravejoy.player.http.filters.SafeHttpLoggingFilter;
 import io.restassured.config.EncoderConfig;
-import io.restassured.config.HttpClientConfig;
 import io.restassured.config.RestAssuredConfig;
 import io.restassured.filter.Filter;
 import io.restassured.filter.log.LogDetail;
@@ -13,13 +12,8 @@ import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.impl.client.HttpClientBuilder;
 
 public final class RequestSpecFactory {
-
-  private static final int CONNECT_TIMEOUT_MS = 5_000;
-  private static final int SOCKET_TIMEOUT_MS = 10_000;
 
   private RequestSpecFactory() {}
 
@@ -50,20 +44,9 @@ public final class RequestSpecFactory {
   }
 
   private static RestAssuredConfig timeoutAndEncodingConfig() {
-    RequestConfig rc =
-        RequestConfig.custom()
-            .setConnectTimeout(CONNECT_TIMEOUT_MS)
-            .setSocketTimeout(SOCKET_TIMEOUT_MS)
-            .setConnectionRequestTimeout(CONNECT_TIMEOUT_MS)
-            .build();
-
     return RestAssuredConfig.config()
         .encoderConfig(
             EncoderConfig.encoderConfig()
-                .appendDefaultContentCharsetToContentTypeIfUndefined(false))
-        .httpClient(
-            HttpClientConfig.httpClientConfig()
-                .httpClientFactory(
-                    () -> HttpClientBuilder.create().setDefaultRequestConfig(rc).build()));
+                .appendDefaultContentCharsetToContentTypeIfUndefined(false));
   }
 }
